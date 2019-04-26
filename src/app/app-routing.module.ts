e42efe03fce2563ca7d53bb4591bc2c5f6a72d11
Tarumã <tarumamiranda@gmail.com>
2019-04-25 22:31:47 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ListaEventosComponent } from './eventos/lista-eventos/lista-eventos.component';
+import { InscricaoComponent } from './usuario/inscricao/inscricao.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'proximos-eventos', component: ListaEventosComponent },
+  { path: 'inscricao', component: InscricaoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+// Bootstrap
+import { CollapseModule } from 'ng2-bootstrap/collapse';
+import { CarouselModule } from 'ng2-bootstrap/carousel';
+
+// Shared components
+import { MenuSuperiorComponent } from './shared/menu-superior/menu-superior.component';
+import { MainPrincipalComponent } from './shared/main-principal/main-principal.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuLoginComponent } from './shared/menu-login/menu-login.component';
+import { ListaEventosComponent } from './eventos/lista-eventos/lista-eventos.component';
+import { InscricaoComponent } from './usuario/inscricao/inscricao.component';
+
+// Services
+import { SeoService } from './services/seo.service';
+import { CustomFormsModule } from 'ng2-validation';
+import { OrganizadorService } from './services/organizador.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuSuperiorComponent,
+    MainPrincipalComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuLoginComponent,
+    ListaEventosComponent,
+    InscricaoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CollapseModule.forRoot(),
+    CarouselModule.forRoot(),
+    ReactiveFormsModule,
+    CustomFormsModule, 
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    Title,
+    SeoService,
+    OrganizadorService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
